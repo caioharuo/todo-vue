2599889985f0322722a03830e0e4dfe0061e8da6
Caio Haruo <caio.haruo@hotmail.com>
2023-09-29 11:09:03 -0300

    init styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,28 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
+    colors: {
+      blue: {
+        500: '#4EA8DE',
+        700: '#1E6F9F',
+      },
+      purple: {
+        500: '#8284FA',
+        700: '#5E60CE',
+      },
+      red: {
+        500: '#E25858',
+      },
+      gray: {
+        700: '#0D0D0D',
+        600: '#1A1A1A',
+        500: '#262626',
+        400: '#333333',
+        300: '#808080',
+        200: '#D9D9D9',
+        100: '#F2F2F2',
+      },
+    },
   },
   plugins: [],
 };
